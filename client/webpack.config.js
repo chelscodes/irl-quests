faf1dc43402fef245bff8a2e9c4cbd4ed63ae8be
Chelsea Smith <chelsea.beth11@gmail.com>
2023-02-27 02:04:06 -0500

    chore: updated app title everywhere

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
       chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css",
     }),
     new HtmlWebpackPlugin({
-      title: "Engage",
+      title: "irlQuests",
       template: path.join(__dirname, "public/index.template.html"),
     }),
   ],
